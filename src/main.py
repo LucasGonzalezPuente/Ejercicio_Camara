5d3a12869536673af9090de58ec217a67dad0c74
Lucas Gonzalez <luucassgon@gmail.com>
2026-03-09 14:31:48 +0100

    FPS y Tasa de Refresco en directo

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 import cv2
 import os
+import time # Librería necesaria para medir el tiempo
 
 
 folder = '../images'
@@ -9,64 +10,47 @@ if not os.path.exists(folder):
 
 cap = cv2.VideoCapture(0)
 
+print("Calculando FPS en tiempo real... Presiona 'q' para salir.")
 
-if not cap.isOpened():
-    print("Error: No se pudo acceder a la webcam.")
-    exit()
 
-print("--- Webcam en VIVO a 100x100 px ---")
-print("Presiona 's' para guardar la foto rescalada.")
-print("Presiona 'q' para salir sin guardar.")
 
 while True:
     
+    start_time = time.time()
+
     ret, frame = cap.read()
     if not ret:
         break
 
-  
-    # Interpolation=cv2.INTER_AREA, el mejor para reducir imágenes(promedia pixeles vecinos)
-    frame_rescalado = cv2.resize(frame, (100, 100), interpolation=cv2.INTER_AREA)
-
-
-    alto_n, ancho_n, _ = frame_rescalado.shape
+    
     
 
-    centro_x = ancho_n // 2
-    centro_y = alto_n // 2
-    punto_centro = (centro_x, centro_y)
+    # Tiempo de refresco: Diferencia entre el tiempo actual y el anterior
+    # FPS = 1 / Tiempo de refresco
+    end_time = time.time()
+    tiempo_refresco = end_time - start_time
+    
+    
+    if tiempo_refresco > 0:
+        fps = 1 / tiempo_refresco
+    else:
+        fps = 0
 
+    texto = f"FPS: {int(fps)} | Refresco: {tiempo_refresco:.4f}s"
+    cv2.putText(frame, texto, (20, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2)
 
     
-    cv2.circle(frame_rescalado, punto_centro, 20, (0, 255, 0), 2)
-    cv2.circle(frame_rescalado, punto_centro, 2, (0, 0, 255), -1)
+    cv2.imshow('Analisis de Rendimiento', frame)
 
-    
-    cv2.imshow('Live a 100x100', frame_rescalado)
-
-    
+   
     key = cv2.waitKey(1) & 0xFF
-    
     if key == ord('s'):
-        
-        img_name = os.path.join(folder, 'captura_100x100.png')
-        cv2.imwrite(img_name, frame_rescalado)
-        
-        print(f"\n" + "="*30)
-        print("DATOS DE LA FOTO GUARDADA")
-        print("="*30)
-        print(f"Resolución guardada: {ancho_n}x{alto_n} px")
-        print(f"Análisis del Array: {frame_rescalado.shape}")
-        print(f"Total de píxeles: {frame_rescalado.size}")
-        print("="*30 + "\n")
-        
-        break 
-        
+        cv2.imwrite(os.path.join(folder, 'captura_fps.png'), frame)
+        print(f"Foto guardada. FPS registrados: {fps:.2f}")
+        break
     elif key == ord('q'):
-        print("Cerrando sin guardar.")
-        break 
+        break
 
-#
+# Limpieza
 cap.release()
 cv2.destroyAllWindows()
-print("Programa finalizado.")
