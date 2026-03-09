647a38c902e694dc901909237669a1ad41e8323b
Lucas Gonzalez <luucassgon@gmail.com>
2026-03-09 13:41:10 +0100

    Realizar foto de la webcam

--- src/main.py
+++ src/main.py
@@ -1,1 +1,38 @@
+import cv2
+import os
+
+folder = '../images'
+
+if not os.path.exists(folder):
+    os.makedirs(folder)
+
+cap = cv2.VideoCapture(0)
+
+if not cap.isOpened():
+    print("ERROR: No se pudo acceder a la camara")
+else:
+    print("Camara detectada. Presiona 's' para hacer una foto o 'q' para salir")
+
+    while True:
+        ret, frame = cap.read()
+
+        if not ret:
+            print("ERROR: No se pudo recibir el frame")
+            break
+
+        cv2.imshow('Previsualizacion - Presiona S para guardar', frame)
+        key = cv2.waitKey(1) & 0xFF
+
+        if key == ord('s'):
+            img_name = os.path.join(folder, "captura_ejercicio.png")
+            cv2.imwrite(img_name, frame)
+            print(f"Imagen guardada en {img_name}")
+
+        elif key == ord('q'):
+            print("Cerrando sin guardar")
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
+
 
